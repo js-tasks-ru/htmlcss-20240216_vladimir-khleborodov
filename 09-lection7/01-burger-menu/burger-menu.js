4d8c5deabcf59a2510eb8f1e9071bd1c17aa4250
Vladimir Khleborodov <khleborodov@hotmail.com>
2024-05-22 19:53:24 +0300

    burger-menu created and mobile pages optimization

--- 09-lection7/01-burger-menu/burger-menu.js
+++ 09-lection7/01-burger-menu/burger-menu.js
@@ -3,17 +3,17 @@ const menuClose = document.getElementById('burger-menu-close');
 const menuOpen = document.getElementById('burger-menu-open');
 
 if (menuOpen) {
-    menuOpen.addEventListener('click', function() {
-        if (menu) {
-            menu.setAttribute('data-open', '');
-        }
-    });
+  menuOpen.addEventListener('click', function() {
+    if (menu) {
+      menu.setAttribute('data-open', '');
+    }
+  });
 }
 
 if (menuClose) {
-    menuClose.addEventListener('click', function() {
-        if (menu) {
-            menu.removeAttribute('data-open');
-        }
-    });
+  menuClose.addEventListener('click', function() {
+    if (menu) {
+      menu.removeAttribute('data-open');
+    }
+  });
 }
